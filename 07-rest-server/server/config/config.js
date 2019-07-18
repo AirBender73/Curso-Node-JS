eac1c35f5dfb082c54927560ca51bc35826d011e
David <daymon.117@gmail.com>
2019-07-18 10:57:08 -0500

    Sección 09: Alcances del REST Server y colección de usuarios
    Creación de base de datos a través de MongoDB 
    Subida de BD a la nube usando Heroku

--- 07-rest-server/server/config/config.js
+++ 07-rest-server/server/config/config.js
@@ -1,3 +1,15 @@
 // PUERTO
-
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+process.env.PORT = process.env.PORT || 3000;
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://david:WZ0Zdv86tF5tKPoU@cluster0-jdbth.mongodb.net/cafe'
+}
+
+process.env.URL_DB = urlDB;
--- 07-rest-server/server/server.js
+++ 07-rest-server/server/server.js
@@ -1,54 +1,30 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-// El app.use significa que son middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-// El app.use significa que son middlewares
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.json('Hello World');
-});
 
-app.post('/usuario', function (req, res) {
+app.use(require('./routes/usuario'));
 
-    let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            usuario: body
-        });
-    }
+mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
-    // res.json(body);
-});
+    if (err) throw err;
 
-app.put('/usuario/:id', function (req, res) {
+    console.log('Base de datos online');
 
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-
-    res.json('put Usuario');
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario');
 });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
-});
+}); 
